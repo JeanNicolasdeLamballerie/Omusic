ce167ccd274a75a74bd318e8c0f525139092f41c
Dekharen <jean.nicolasdelamballerie@gmail.com>
2022-02-03 13:49:49 +0100

    added client to login state

--- lib/login_provider.dart
+++ lib/login_provider.dart
@@ -3,7 +3,6 @@ import 'package:google_sign_in/google_sign_in.dart';
 import 'package:extension_google_sign_in_as_googleapis_auth/extension_google_sign_in_as_googleapis_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:omusic/login.dart';
-import 'package:googleapis_auth/googleapis_auth.dart' as auth show AuthClient;
 //import 'dart:convert' show json;
 
 GoogleSignIn _googleSignIn = GoogleSignIn(
